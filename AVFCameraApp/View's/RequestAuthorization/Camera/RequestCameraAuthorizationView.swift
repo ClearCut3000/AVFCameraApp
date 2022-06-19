fb7578164e8ffc07f5bd9049cfcc9cd3919df541
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-06-19 22:10:06 +0300

    Added animations for launch View and for requestAuthorization View

--- AVFCameraApp/View's/RequestAuthorization/Camera/RequestCameraAuthorizationView.swift
+++ AVFCameraApp/View's/RequestAuthorization/Camera/RequestCameraAuthorizationView.swift
@@ -22,6 +22,7 @@ class RequestCameraAuthorizationView: UIView {
   @IBOutlet private weak var titleLabel: UILabel!
   @IBOutlet private weak var messageLabel: UILabel!
   @IBOutlet private weak var actionButton: UIButton!
+  @IBOutlet private weak var actionButtonWithConstraint: NSLayoutConstraint!
 
   //MARK: - Init's
   override init(frame: CGRect) {
@@ -35,7 +36,6 @@ class RequestCameraAuthorizationView: UIView {
     fatalError("init(coder:) has not been implemented")
   }
 
-  //MARK: - Method's
   private func customInit() {
     let bundle = Bundle.main
     let nibName = String(describing: Self.self)
@@ -43,10 +43,71 @@ class RequestCameraAuthorizationView: UIView {
     addSubview(contentView)
     contentView.frame = bounds
     contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    addActionButtonShadow()
+  }
+
+  //MARK: - Method's
+  private func addActionButtonShadow() {
+    actionButton.layer.shadowColor = UIColor.black.cgColor
+    actionButton.layer.shadowRadius = 10
+    actionButton.layer.opacity = 0.3
+    actionButton.layer.masksToBounds = false
+    actionButton.layer.shadowOffset = CGSize(width: 5, height: 5)
+    actionButton.layer.cornerRadius = 4
+  }
+
+  func animateInViews() {
+    let viewsToAnimate = [cameraImageView, titleLabel, messageLabel, actionButton]
+    for (i, viewToAnimate) in viewsToAnimate.enumerated() {
+      guard let view = viewToAnimate else { continue }
+      animateInView(view: view, delay: Double(i) * 0.15)
+    }
+  }
+
+  func animateOutViews(completion: @escaping () -> Void) {
+    let viewsToAnimate = [cameraImageView, titleLabel, messageLabel, actionButton]
+    for (i, viewToAnimate) in viewsToAnimate.enumerated() {
+      guard let view = viewToAnimate else { continue }
+      var completionHandler: (() -> Void)? = nil
+      if viewToAnimate == viewsToAnimate.last {
+        completionHandler = completion
+      }
+      animateOutView(view: view, delay: Double(i) * 0.15, completion: completionHandler)
+    }
+  }
+
+  func configureForDisableState() {
+    titleLabel.text = "Camera Authorization Denied!"
+    actionButton.setTitle("Open Settings", for: .normal)
+    actionButtonWithConstraint.constant = 120
   }
 
   //MARK: - Action's
   @IBAction func actionButtonHandler(button: UIButton) {
     delegate?.requestCameraAuthorizationTapped()
+  }
+}
+
+//MARK: - Animation's
+private extension RequestCameraAuthorizationView {
+  func animateInView(view: UIView, delay: TimeInterval) {
+    view.alpha = 0
+    view.transform = CGAffineTransform(translationX: 0, y: -20)
+    let animation = UIViewPropertyAnimator(duration: 0.2, curve: .easeInOut) {
+      view.alpha = 1
+      view.transform = .identity
+    }
+    animation.startAnimation(afterDelay: delay)
+  }
+
+  func animateOutView(view: UIView, delay: TimeInterval, completion: (() -> Void)? = nil) {
+    let animation = UIViewPropertyAnimator(duration: 0.2, curve: .easeInOut) {
+      view.alpha = 0
+      view.transform = CGAffineTransform(translationX: 0, y: -20)
+    }
+    animation.addCompletion { _ in
+      completion?()
+    }
+    animation.startAnimation(afterDelay: delay)
   }
 }
--- AVFCameraApp/ViewControllers/Launch/LaunchViewController.swift
+++ AVFCameraApp/ViewControllers/Launch/LaunchViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class LaunchViewController: UIViewController {
 
+  //MARK: - Propertie's
+  private var requestCameraAuthorizationView: RequestCameraAuthorizationView?
+
   //MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    setupViews()
+    setupViewForNextAuthorizationRequest()
   }
 }
 
 //MARK: - Setting View
 private extension LaunchViewController {
-  func setupViews() {
+  func setupViewForNextAuthorizationRequest() {
+
+  }
+
+  func setupRequestCameraAuthorizationView() {
     let requestCameraAuthorizationView = RequestCameraAuthorizationView()
     requestCameraAuthorizationView.translatesAutoresizingMaskIntoConstraints = false
     requestCameraAuthorizationView.delegate = self
@@ -29,6 +36,18 @@ private extension LaunchViewController {
       requestCameraAuthorizationView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
       requestCameraAuthorizationView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
     ])
+    requestCameraAuthorizationView.animateInViews()
+    self.requestCameraAuthorizationView = requestCameraAuthorizationView
+  }
+
+  func removeRequestCameraAuthorizationView() {
+    guard let requestCameraAuthorizationView = requestCameraAuthorizationView else { return }
+    requestCameraAuthorizationView.animateOutViews(completion: { [weak self] in
+      guard let self = self else { return }
+      requestCameraAuthorizationView.removeFromSuperview()
+      self.requestCameraAuthorizationView = nil
+      self.setupViewForNextAuthorizationRequest()
+    })
   }
 }
 
